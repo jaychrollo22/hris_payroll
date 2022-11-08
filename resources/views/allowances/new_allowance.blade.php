ad76ad653c89dd2302e1197ef0de44b7425393a0
RDMontana06 <montanareymart06@gmail.com>
2022-11-08 13:54:56 +0800

    Fix Spelling

--- resources/views/allowances/new_allowance.blade.php
+++ resources/views/allowances/new_allowance.blade.php
@@ -3,7 +3,7 @@
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="newAllowancelabel">New Alowance</h5>
+				<h5 class="modal-title" id="newAllowancelabel">New Allowance</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
