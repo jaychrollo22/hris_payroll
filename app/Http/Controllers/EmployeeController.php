a7e541bf9c6cd131dd7bfc5aa35c85d4e9f643a5
renzcabato1 <rccabato@premiummegastructures.com>
2022-12-02 08:49:09 +0800

    Done

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -195,7 +195,7 @@ class EmployeeController extends Controller
             $company_employees = EmployeeCompany::where('company_id',$request->company)->pluck('emp_code')->toArray();
             $emp_data = PersonnelEmployee::with(['attendances' => function ($query) use ($from_date, $to_date) {
                 $query->whereBetween('time_in', [$from_date." 00:00:01", $to_date." 23:59:59"])->orWhereBetween('time_out', [$from_date." 00:00:01", $to_date." 23:59:59"])
-                ->orderBy('time_in','asc')->orderBy('id','asc');
+                ->orderBy('time_in','asc')->orderby('time_out','desc')->orderBy('id','asc');
             }])->whereIn('emp_code', $company_employees)->get();
             // dd($company_employees);
             $schedules = ScheduleData::where('schedule_id', 1)->get();
