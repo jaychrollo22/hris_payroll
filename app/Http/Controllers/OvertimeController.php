a0bcd735674ac1b792fae83e7b33abae276e5863
chrollo <jay.lumagdong@gmail.com>
2023-02-22 01:20:50 +0800

    patch update : overtime report

--- app/Http/Controllers/OvertimeController.php
+++ app/Http/Controllers/OvertimeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\EmployeeApproverController;
+use App\Company;
 use App\Employee;
 use App\Overtime;
+use App\EmployeeCompany;
 use App\EmployeeOvertime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,13 +14,30 @@ use RealRashid\SweetAlert\Facades\Alert;
 class OvertimeController extends Controller
 {
     //
-    public function index(){
+    public function index(Request $request){
+        $from_date = $request->from;
+        $to_date = $request->to;
+        $date_range = '';
+        $companies = Company::whereHas('employee_company')->get();
 
-        $employee_overtimes = EmployeeOvertime::with('user','employee')->where('status','Approved')->get();
+        $employee_overtimes=[];
+        if ($from_date != null) {
+            $company_employees = Employee::where('company_id',$request->company)->pluck('user_id')->toArray();
+            
+            $employee_overtimes = EmployeeOvertime::with('user','employee')->where(function ($query) use ($from_date, $to_date) {
+                $query->whereBetween('ot_date', [$from_date." 00:00:01", $to_date." 23:59:59"])
+                ->orderBy('ot_date','asc')->orderby('user_id','desc')->orderBy('id','asc');
+            })->whereIn('user_id', $company_employees)->get();
+        }
+
         return view('overtimes.index',
         array(
             'header' => 'overtimes',
             'employee_overtimes' => $employee_overtimes,
+            'companies' => $companies,
+            'from_date' => $from_date,
+            'to_date' => $to_date,
+            'date_range' => $date_range,
         ));
     }
 
--- resources/views/overtimes/index.blade.php
+++ resources/views/overtimes/index.blade.php
@@ -7,10 +7,52 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">Overtime Reports</h4>
+                        <p class="card-description">
+                            <form method='get' onsubmit='show();' enctype="multipart/form-data">
+                                <div class=row>
+                                    <div class='col-md-3'>
+                                        <div class="form-group row">
+                                            <label class="col-sm-4 col-form-label text-right">Company</label>
+                                            <div class="col-sm-8">
+                                                <select data-placeholder="Select Company" class="form-control form-control-sm required js-example-basic-single" style='width:100%;' name='company' required>
+                                                    <option value="">-- Select Employee --</option>
+                                                    @foreach($companies as $comp)
+                                                    <option value="{{$comp->id}}">{{$comp->company_name}} - {{$comp->company_code}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-3'>
+                                        <div class="form-group row">
+                                            <label class="col-sm-4 col-form-label text-right">From</label>
+                                            <div class="col-sm-8">
+                                                <input type="date" value='{{$from_date}}' class="form-control" name="from" max='{{date('Y-m-d')}}' onchange='get_min(this.value);' required />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-3'>
+                                        <div class="form-group row">
+                                            <label class="col-sm-4 col-form-label text-right">To</label>
+                                            <div class="col-sm-8">
+                                                <input type="date" value='{{$to_date}}' class="form-control" name="to" id='to' max='{{date('Y-m-d')}}' required />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-3'>
+                                        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </p>
+                        @if(count($employee_overtimes) > 0)
+                        <button class='btn btn-info mb-1' onclick="exportTableToExcel('overtime_report','Overtime Report {{$from_date}} - {{$to_date}}')">Export</button>
+                        @endif
                         <div class="table-responsive">
-                            <table class="table table-hover table-bordered tablewithSearch">
+                            <table class="table table-hover table-bordered tablewithSearch" id="overtime_report">
                                 <thead>
                                     <tr>
+                                        <th>User ID</th>
                                         <th>Biometric ID</th>
                                         <th>Name</th>
                                         <th>Date</th>
@@ -22,6 +64,7 @@
                                 <tbody>
                                     @foreach($employee_overtimes as $item)
                                     <tr>
+                                        <td>{{$item->employee->user_id}}</td>
                                         <td>{{$item->employee->employee_number}}</td>
                                         <td>{{$item->user->name}}</td>
                                         <td>{{$item->ot_date}}</td>
