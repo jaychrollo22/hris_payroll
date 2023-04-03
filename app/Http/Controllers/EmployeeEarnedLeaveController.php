2b1abaca4e27a67bd9f564aaeb0979f4dba0e787
chrollo <jay.lumagdong@gmail.com>
2023-04-03 16:02:13 +0800

    employee earned

--- app/Http/Controllers/EmployeeEarnedLeaveController.php
+++ app/Http/Controllers/EmployeeEarnedLeaveController.php
@@ -8,8 +8,13 @@ use App\EmployeeEarnedLeave;
 class EmployeeEarnedLeaveController extends Controller
 {
     public function index(){
-
+        $allowed_companies = getUserAllowedCompanies(auth()->user()->id);
         $earned_leaves = EmployeeEarnedLeave::with('employee.company','leave_type_info')
+                                                ->whereHas('employee',function($q) use($allowed_companies){
+                                                    $q->whereHas('company',function($w) use($allowed_companies){
+                                                        return $w->whereIn('company_id',$allowed_companies);
+                                                    });
+                                                })
                                                 ->orderBy('user_id','ASC')
                                                 ->orderBy('leave_type','ASC')
                                                 ->orderBy('created_at','DESC')
