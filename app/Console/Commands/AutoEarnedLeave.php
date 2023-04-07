31290de7ac8a27e6220c8ea7ec3e10752a0f9700
chrollo <jay.lumagdong@gmail.com>
2023-04-07 08:22:28 +0800

    leave earned 01

--- app/Console/Commands/AutoEarnedLeave.php
+++ app/Console/Commands/AutoEarnedLeave.php
@@ -50,7 +50,7 @@ class AutoEarnedLeave extends Command
     public function getEmployeeEarnedVacationLeaves(){
 
         $month = date('m');
-        $day = date('01');
+        $day = date('02');
         $classifications = [1,2,3,5];
 
         $companies = [14,11,7];
@@ -117,7 +117,7 @@ class AutoEarnedLeave extends Command
     public function getEmployeeEarnedSickLeaves(){
 
         $month = date('m');
-        $day = date('01');
+        $day = date('02');
         $classifications = [1,2,3];
         $companies = [14,11,7];
 
