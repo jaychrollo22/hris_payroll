8e257542cb412ecec47c841332384dba52d2e062
chrollo <jay.lumagdong@gmail.com>
2023-04-25 08:50:53 +0800

    employee attendance

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -34,6 +34,14 @@ class AttendanceController extends Controller
         }
         $schedules = ScheduleData::where('schedule_id',auth()->user()->employee->schedule_id)->get();
         // dd($attendances);
+
+        $emp_data = Employee::with(['attendances' => function ($query) use ($from_date, $to_date) {
+                                $query->whereBetween('time_in', [$from_date." 00:00:01", $to_date." 23:59:59"])->orWhereBetween('time_out', [$from_date." 00:00:01", $to_date." 23:59:59"])
+                                ->orderBy('time_in','asc')->orderby('time_out','desc')->orderBy('id','asc');
+                            }])
+                            ->where('employee_number', auth()->user()->employee->employee_number)
+                            ->get();
+
         return view('attendances.view_attendance',
         array(
             'header' => 'attendances',
@@ -42,6 +50,7 @@ class AttendanceController extends Controller
             'date_range' => $date_range,
             'attendances' => $attendances,
             'schedules' => $schedules,
+            'emp_data' => $emp_data,
         ));
     }
     public function subordinates(Request $request)
