1fd4d8dbe8c18de4a173c454dece7c5532d1ca48
chrollo <jay.lumagdong@gmail.com>
2023-05-09 22:45:36 +0800

    leave report - new remarks

--- resources/views/exports/leave_export.blade.php
+++ resources/views/exports/leave_export.blade.php
@@ -21,10 +21,29 @@
                     <td>{{$leave->employee->first_name . ' ' . $leave->employee->last_name}}</td>
                     <td>{{date('d/m/Y',strtotime($date_r))}}</td>
                     <td>{{$leave->leave->leave_type}}</td>
-                    <td>{{$leave->withpay == 1 ? 'With-Pay' : 'Without-Pay'}}</td>
+                    <td>
+                        {{-- {{$leave->withpay == 1 ? 'With-Pay' : 'Without-Pay'}} --}}
+                        @php
+                            $remarks = '';   
+                            if($leave->withpay == '1'){
+                                $remarks = 'Full-Pay';
+                                if($leave->halfday == '1'){
+                                    if($leave->halfday_status == 'First Shift'){
+                                        $remarks = 'Half-Pay (FS)';
+                                    }
+                                    else if($leave->halfday_status == 'Second Shift'){
+                                        $remarks = 'Half-Pay (SS)';
+                                    }
+                                }
+                            }else{
+                                $remarks = 'Without-Pay';
+                            }
+                        @endphp
+                        {{$remarks}}
+                    </td>
                 </tr>
                 @endforeach
             </tbody>
             @endif
-        @endforeach>
+        @endforeach
 </table>
