3b64b33a9b68aa1771638d8c60921f3b951c7a5a
chrollo <jay.lumagdong@gmail.com>
2023-06-05 09:40:39 +0800

    patch update : subordinates

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -58,31 +58,71 @@ class AttendanceController extends Controller
     }
     public function subordinates(Request $request)
     {
-        //  
+        $attendance_controller = new AttendanceController; 
         $from_date = $request->from;
         $to_date = $request->to;
         $date_range =  [];
         $attendances = [];
         $schedules = [];
-        if($from_date != null)
-        {
-        $date_range =  $this->dateRange( $from_date, $to_date);
-        $attendances =  $this->get_attendances($from_date,$to_date,$request->employee);
-        $schedule_id = Employee::where('employee_number',$request->employee)->first();
-        // dd($schedule_id);
-        $schedules = ScheduleData::where('schedule_id',$schedule_id->schedule_id)->get();
+        $emp_code = $request->employee;
+        $schedule_id = null;
+        $emp_data = [];
+        if ($from_date != null) {
+            $emp_data = Employee::with(['attendances' => function ($query) use ($from_date, $to_date) {
+                                            $query->whereBetween('time_in', [$from_date." 00:00:01", $to_date." 23:59:59"])
+                                                    ->orWhereBetween('time_out', [$from_date." 00:00:01", $to_date." 23:59:59"])
+                                                    ->orderBy('time_in','asc')
+                                                    ->orderby('time_out','desc')
+                                                    ->orderBy('id','asc');
+                                    }])
+                                    ->whereIn('employee_number', $request->employee)
+                                    ->get();
+
+            $date_range =  $attendance_controller->dateRange($from_date, $to_date);
+           
         }
+        $schedules = ScheduleData::all();
+        
+        return view(
+            'attendances.subordinates_attendances',
+            array(
+                'header' => 'subordinates',
+                // 'employees' => $employees,
+                'from_date' => $from_date,
+                'to_date' => $to_date,
+                'date_range' => $date_range,
+                'attendances' => $attendances,
+                'schedules' => $schedules,
+                'emp_code' => $emp_code,
+                'emp_data' => $emp_data,
+            )
+        );
+
+        // //  
+        // $from_date = $request->from;
+        // $to_date = $request->to;
+        // $date_range =  [];
+        // $attendances = [];
+        // $schedules = [];
+        // if($from_date != null)
+        // {
+        //     $date_range =  $this->dateRange( $from_date, $to_date);
+        //     $attendances =  $this->get_attendances($from_date,$to_date,$request->employee);
+        //     $schedule_id = Employee::where('employee_number',$request->employee)->first();
+        //     // dd($schedule_id);
+        //     $schedules = ScheduleData::where('schedule_id',$schedule_id->schedule_id)->get();
+        // }
     
-        // dd($attendances);
-        return view('attendances.subordinates_attendances',
-        array(
-            'header' => 'subordinates',
-            'from_date' => $from_date,
-            'to_date' => $to_date,
-            'date_range' => $date_range,
-            'attendances' => $attendances,
-            'schedules' => $schedules,
-        ));
+        // // dd($attendances);
+        // return view('attendances.subordinates_attendances',
+        // array(
+        //     'header' => 'subordinates',
+        //     'from_date' => $from_date,
+        //     'to_date' => $to_date,
+        //     'date_range' => $date_range,
+        //     'attendances' => $attendances,
+        //     'schedules' => $schedules,
+        // ));
     }
     
     public function dateRange( $first, $last, $step = '+1 day', $format = 'Y-m-d' ) {
