eb8205a36faa9251b01646a2d0ca9473bb78287a
chrollo <jay.lumagdong@gmail.com>
2023-06-19 11:08:06 +0800

    patch update : manual earned leaves

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -535,7 +535,10 @@
                         <a class="nav-link" href="{{ url('/employee-leave-credits') }}">Employee Leave Credits</a>
                     </li>
                     @endif
-                    @if(checkUserPrivilege('masterfiles_employee_leave_credits',auth()->user()->id) == 'yes' && auth()->user()->id == '1')
+                    @if(checkUserPrivilege('masterfiles_employee_leave_credits',auth()->user()->id) == 'yes' && (auth()->user()->id == '1' || auth()->user()->id == '660'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/manual-employee-earned-leaves') }}">Manual Earned Leaves</a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('/employee-leave-balances') }}">Employee Leave Balances</a>
                     </li>
