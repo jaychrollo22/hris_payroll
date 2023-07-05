d890614bab2da459c682ec558c42f425e182438c
chrollo <jay.lumagdong@gmail.com>
2023-07-05 11:13:09 +0800

    main

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,12 @@ use App\Announcement;
 use App\ScheduleData;
 use App\Holiday;
 
+use App\EmployeeLeave;
+use App\EmployeeOvertime;
+use App\EmployeeWfh;
+use App\EmployeeOb;
+use App\EmployeeDtr;
+
 class HomeController extends Controller
 {
     /**
@@ -68,7 +74,13 @@ class HomeController extends Controller
         ->orderBy('holiday_date','asc')->get();
         // dd($holidays);
 
-        
+        session([
+            'pending_leave_count'=>$this->pending_leave_count(auth()->user()->id),
+            'pending_overtime_count'=>$this->pending_overtime_count(auth()->user()->id),
+            'pending_wfh_count'=>$this->pending_wfh_count(auth()->user()->id),
+            'pending_ob_count'=>$this->pending_ob_count(auth()->user()->id),
+            'pending_dtr_count'=>$this->pending_dtr_count(auth()->user()->id),
+        ]);
 
         return view('dashboards.home',
         array(
@@ -95,5 +107,83 @@ class HomeController extends Controller
             'header' => 'dashboard-manager',
             'handbook' => $handbook,
         ));
+    }
+
+    public function pending_leave_count($approver_id){
+
+        $today = date('Y-m-d');
+        $from_date = date('Y-m-d',(strtotime ( '-1 month' , strtotime ( $today) ) ));
+        $to_date = date('Y-m-d');
+    
+        return EmployeeLeave::with('approver.approver_info','user')
+                                    ->whereHas('approver',function($q) use($approver_id) {
+                                        $q->where('approver_id',$approver_id);
+                                    })
+                                    ->whereDate('created_at','>=',$from_date)
+                                    ->whereDate('created_at','<=',$to_date)
+                                    ->where('status','Pending')
+                                    ->count();
+    }
+    public function pending_overtime_count($approver_id){
+    
+        $today = date('Y-m-d');
+        $from_date = date('Y-m-d',(strtotime ( '-1 month' , strtotime ( $today) ) ));
+        $to_date = date('Y-m-d');
+    
+        return EmployeeOvertime::with('approver.approver_info','user')
+                                    ->whereHas('approver',function($q) use($approver_id) {
+                                        $q->where('approver_id',$approver_id);
+                                    })
+                                    ->whereDate('created_at','>=',$from_date)
+                                    ->whereDate('created_at','<=',$to_date)
+                                    ->where('status','Pending')
+                                    ->count();
+    }
+    public function pending_wfh_count($approver_id){
+    
+        $today = date('Y-m-d');
+        $from_date = date('Y-m-d',(strtotime ( '-1 month' , strtotime ( $today) ) ));
+        $to_date = date('Y-m-d');
+    
+        return EmployeeWfh::with('approver.approver_info','user')
+                                    ->whereHas('approver',function($q) use($approver_id) {
+                                        $q->where('approver_id',$approver_id);
+                                    })
+                                    ->where('status','Pending')
+                                    ->whereDate('created_at','>=',$from_date)
+                                    ->whereDate('created_at','<=',$to_date)
+                                    ->count();
+    }
+    
+    public function pending_ob_count($approver_id){
+    
+        $today = date('Y-m-d');
+        $from_date = date('Y-m-d',(strtotime ( '-1 month' , strtotime ( $today) ) ));
+        $to_date = date('Y-m-d');
+    
+        return EmployeeOb::with('approver.approver_info','user')
+                                    ->whereHas('approver',function($q) use($approver_id) {
+                                        $q->where('approver_id',$approver_id);
+                                    })
+                                    ->where('status','Pending')
+                                    ->whereDate('created_at','>=',$from_date)
+                                    ->whereDate('created_at','<=',$to_date)
+                                    ->count();
+    }
+    
+    public function pending_dtr_count($approver_id){
+    
+        $today = date('Y-m-d');
+        $from_date = date('Y-m-d',(strtotime ( '-1 month' , strtotime ( $today) ) ));
+        $to_date = date('Y-m-d');
+    
+        return EmployeeDtr::with('approver.approver_info','user')
+                                    ->whereHas('approver',function($q) use($approver_id) {
+                                        $q->where('approver_id',$approver_id);
+                                    })
+                                    ->where('status','Pending')
+                                    ->whereDate('created_at','>=',$from_date)
+                                    ->whereDate('created_at','<=',$to_date)
+                                    ->count();
     }
 }
