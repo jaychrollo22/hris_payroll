d86e2a61c1de13e541f4a24d8960bad3ae6374ec
Renz Christian M. Cabato <rmcabato@wlandmark.com.ph>
2023-12-12 17:57:24 +0800

    Done

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,22 +14,22 @@
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
     <!-- plugins:css -->
-    <link rel="stylesheet" href="{{ asset('body_css/vendors/feather/feather.css') }}">
-    <link rel="stylesheet" href="{{ asset('body_css/vendors/ti-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('body_css/vendors/css/vendor.bundle.base.css') }}">
+    <link rel="stylesheet" href="{{ asset('/body_css/vendors/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('/body_css/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('/body_css/vendors/css/vendor.bundle.base.css') }}">
     <!-- endinject -->
     @yield('css_header')
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="{{ asset('body_css/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
-    <link rel="stylesheet" href="{{ asset('body_css/vendors/ti-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('body_css/js/select.dataTables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/body_css/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('/body_css/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/body_css/js/select.dataTables.min.css') }}">
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="{{ asset('body_css/vendors/select2/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('body_css/vendors/select2-bootstrap-theme/select2-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/body_css/vendors/select2/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/body_css/vendors/select2-bootstrap-theme/select2-bootstrap.min.css') }}">
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.6.6/dist/sweetalert2.min.css"> --}}
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('body_css/css/vertical-layout-light/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/body_css/css/vertical-layout-light/style.css') }}">
     <!-- endinject -->
 
     <!-- Fonts -->
@@ -621,44 +621,44 @@
 
     @include('sweetalert::alert')
     <!-- plugins:js -->
-    <script src="{{ asset('body_css/vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('/body_css/vendors/js/vendor.bundle.base.js') }}"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
-    <script src="{{ asset('body_css/vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('/body_css/vendors/chart.js/Chart.min.js') }}"></script>
 
-    <script src="{{ asset('body_css/vendors/select2/select2.min.js') }}"></script>
+    <script src="{{ asset('/body_css/vendors/select2/select2.min.js') }}"></script>
     <!-- End plugin js for this page -->
     <!-- inject:js -->
 
     <!-- endinject -->
     <!-- Custom js for this page-->
-    <script src="{{ asset('body_css/js/dashboard.js') }}"></script>
-    <script src="{{ asset('body_css/js/select2.js') }}"></script>
+    <script src="{{ asset('/body_css/js/dashboard.js') }}"></script>
+    <script src="{{ asset('/body_css/js/select2.js') }}"></script>
 
 
-    <script src="{{ asset('body_css/vendors/datatables.net/jquery.dataTables.js') }}"></script>
-    <script src="{{ asset('body_css/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
-    <script src="{{ asset('body_css/vendors/jquery.repeater/jquery.repeater.min.js') }}"></script>
+    <script src="{{ asset('/body_css/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('/body_css/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('/body_css/vendors/jquery.repeater/jquery.repeater.min.js') }}"></script>
 
-    <script src="{{ asset('body_css/js/dataTables.select.min.js') }}"></script>
+    <script src="{{ asset('/body_css/js/dataTables.select.min.js') }}"></script>
 
-    <script src="{{ asset('body_css/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('body_css/js/hoverable-collapse.js') }}"></script>
-    <script src="{{ asset('body_css/js/template.js') }}"></script>
-    <script src="{{ asset('body_css/js/settings.js') }}"></script>
-    <script src="{{ asset('body_css/js/todolist.js') }}"></script>
+    <script src="{{ asset('/body_css/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('/body_css/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('/body_css/js/template.js') }}"></script>
+    <script src="{{ asset('/body_css/js/settings.js') }}"></script>
+    <script src="{{ asset('/body_css/js/todolist.js') }}"></script>
 
-    <script src="{{ asset('body_css/js/tabs.js') }}"></script>
-    <script src="{{ asset('body_css/js/form-repeater.js') }}"></script>
-    <script src="{{ asset('body_css/vendors/sweetalert/sweetalert.min.js') }}"></script>
+    <script src="{{ asset('/body_css/js/tabs.js') }}"></script>
+    <script src="{{ asset('/body_css/js/form-repeater.js') }}"></script>
+    <script src="{{ asset('/body_css/vendors/sweetalert/sweetalert.min.js') }}"></script>
 
-    <script src="{{ asset('body_css/vendors/inputmask/jquery.inputmask.bundle.js') }}"></script>
-    <script src="{{ asset('body_css/vendors/inputmask/jquery.inputmask.bundle.js') }}"></script>
-    <script src="{{ asset('body_css/js/inputmask.js') }}"></script>
+    <script src="{{ asset('/body_css/vendors/inputmask/jquery.inputmask.bundle.js') }}"></script>
+    <script src="{{ asset('/body_css/vendors/inputmask/jquery.inputmask.bundle.js') }}"></script>
+    <script src="{{ asset('/body_css/js/inputmask.js') }}"></script>
 
     {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.6.6/dist/sweetalert2.all.min.js"></script> --}}
-    {{-- <script src="{{ asset('body_css/js/form-validation.js') }}"></script>
-    <script src="{{ asset('body_css/js/bt-maxLength.js') }}"></script> --}}
+    {{-- <script src="{{ asset('/body_css/js/form-validation.js') }}"></script>
+    <script src="{{ asset('/body_css/js/bt-maxLength.js') }}"></script> --}}
     @yield('footer')
     <script>
         var span = document.getElementById('span');
