689d4140b0f3ffb3bce31c460138c038e07542a1
Arjay Lumagdong <jay.lumagdong@gmail.com>
2024-01-15 16:03:43 +0800

    location

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -520,7 +520,7 @@
                             {{-- </ul> --}}
                         {{-- </div> --}}
         </li>
-        @if (checkUserPrivilege('masterfiles_companies',auth()->user()->id) == 'yes' || checkUserPrivilege('masterfiles_departments',auth()->user()->id) == 'yes' || checkUserPrivilege('masterfiles_loan_types',auth()->user()->id) == 'yes' || checkUserPrivilege('masterfiles_employee_leave_credits',auth()->user()->id) == 'yes')
+        @if (checkUserPrivilege('masterfiles_companies',auth()->user()->id) == 'yes' || checkUserPrivilege('masterfiles_locations',auth()->user()->id) == 'yes' || checkUserPrivilege('masterfiles_departments',auth()->user()->id) == 'yes' || checkUserPrivilege('masterfiles_loan_types',auth()->user()->id) == 'yes' || checkUserPrivilege('masterfiles_employee_leave_credits',auth()->user()->id) == 'yes')
         <li class="nav-item @if ($header == 'masterfiles') active @endif">
             <a class="nav-link" data-toggle="collapse" href="#masterfiles" aria-expanded="false" aria-controls="ui-basic">
                 <i class="icon-align-center menu-icon"></i>
