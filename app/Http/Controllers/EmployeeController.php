7ed306193a1ccdc9dc4af404b0f544735b852149
Arjay Lumagdong <jay.lumagdong@gmail.com>
2024-02-20 11:32:09 +0800

    patch update : add location

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1420,7 +1420,7 @@ class EmployeeController extends Controller
             
             
 
-            $emp_data = Employee::select('id','user_id','employee_number','first_name','last_name','schedule_id')
+            $emp_data = Employee::select('id','user_id','employee_number','first_name','last_name','schedule_id','location')
                                     ->with(['schedule_info','attendances' => function ($query) use ($from_date, $to_date) {
                                             $query->whereBetween('time_in', [$from_date." 00:00:01", $to_date." 23:59:59"])
                                                     ->orWhereBetween('time_out', [$from_date." 00:00:01", $to_date." 23:59:59"])
