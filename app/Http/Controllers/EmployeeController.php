7299f6fd6fc26bd26f23e549864aeee85218f5a3
Arjay Lumagdong <jay.lumagdong@gmail.com>
2024-04-14 07:25:15 +0800

    cross company approval

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1041,11 +1041,12 @@ class EmployeeController extends Controller
         $employees = Employee::with('department', 'payment_info', 'ScheduleData', 'immediate_sup_data', 'user_info', 'company','classification_info','level_info')->get();
         
         
-        $employee_approvers = Employee::whereHas('company',function($q) use($user){
-                                                if($user->employee->company_id){
-                                                    $q->where('company_id',$user->employee->company_id);
-                                                }
-                                        })
+        $employee_approvers = Employee::where('classification','!=','1')
+                                        // whereHas('company',function($q) use($user){
+                                        //         if($user->employee->company_id){
+                                        //             $q->where('company_id',$user->employee->company_id);
+                                        //         }
+                                        // })
                                         ->where('status','Active')
                                         ->pluck('user_id')
                                         ->toArray();
