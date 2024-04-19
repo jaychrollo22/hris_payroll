abd286a00a483d3b491d7754586593e98156b93d
Arjay Lumagdong <jay.lumagdong@gmail.com>
2024-04-19 08:18:11 +0800

    update user

--- app/User.php
+++ app/User.php
@@ -6,8 +6,13 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+use OwenIt\Auditing\Contracts\Auditable;
+
+class User extends Authenticatable implements Auditable
 {
+    
+    use \OwenIt\Auditing\Auditable;
+
     use Notifiable;
 
     /**
