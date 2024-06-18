3e5b873128d51b5495f33bdf1e6f4d2b3c85c91d
Renz Christian Cabato <cabato.renz.renz@gmail.com>
2024-06-18 11:22:38 +0800

    MRDC Back

--- resources/views/employees/7.blade.php
+++ resources/views/employees/7.blade.php
@@ -135,10 +135,10 @@
     </div>
     <div class="container">
    
-        <img style='border:1px;' src="{{asset('/images/MRDCBack.jpg')}}" width="100%" height="100%"/>  </div>
+        <img style='border:1px;' src="{{asset('/images/MRDCBack.png')}}" width="100%" height="100%"/>
         <div class="qr-code" ><img src="data:image/png;base64, {!! base64_encode(QrCode::format('svg')->size(200)->errorCorrection('H')->generate('https://hris.wsystem.online/calling-card/'.$employee->employee_code)) !!}"></div>
     </div>
-    </body>
+</body>
 </html>
 
 
