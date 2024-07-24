886b4fa9d70a519de7475cb28da077dc95199147
Arjay Lumagdong <jay.lumagdong@gmail.com>
2024-07-24 14:52:44 +0800

    update approver

--- resources/views/employee_performance_evaluations/hr_index.blade.php
+++ resources/views/employee_performance_evaluations/hr_index.blade.php
@@ -233,7 +233,7 @@
                             @endif
                           </td>
                           <td id="tdStatus{{ $eval->id }}">
-                            @if ($eval->approver)
+                            @if (count($eval->approver) > 0)
                               {{$eval->approver[0]->approver_info ? $eval->approver[0]->approver_info->name : ""}}
                             @endif
                           </td>
