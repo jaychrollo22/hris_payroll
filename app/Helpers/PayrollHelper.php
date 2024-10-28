4073feb15a9c7395ee886cbe9fb9aa97285f3d5c
Arjay Lumagdong <jay.lumagdong@gmail.com>
2024-10-28 08:54:48 +0800

    employee_share_ee

--- app/Helpers/PayrollHelper.php
+++ app/Helpers/PayrollHelper.php
@@ -85,10 +85,12 @@ function getUserOvertime($user_id,$payroll_period_id){
         ->sum('total_overtime_pay');
 }
 
-function getUserAbsencesAmount($user_id,$payroll_period_id){
-    return PayrollAttendance::where('payroll_period_id',$payroll_period_id)
-    ->where('user_id',$user_id)
-    ->sum('absences_amount');
+function getUserNoOfDaysWorked($user_id,$payroll_period_id){
+    $payroll_attendance = PayrollAttendance::select('no_of_days_worked')->where('payroll_period_id',$payroll_period_id)
+                                                ->where('user_id',$user_id)->first();
+    if($payroll_attendance){
+        return $payroll_attendance->no_of_days_worked;
+    }
 }
 
 function getUserLatesAmount($user_id,$payroll_period_id){
