5c3b8e52813a574388c8fa1c2135c2cd9f1dae6e
Arjay Lumagdong <jay.lumagdong@gmail.com>
2024-12-12 17:45:39 +0800

    update deduction

--- app/Helpers/DeductionHelper.php
+++ app/Helpers/DeductionHelper.php
@@ -2,10 +2,10 @@
 use App\EmployeeDeduction;
 
 function getUserDeductionAmount($user_id,$deduction_id,$cut_off){
-    return EmployeeDeduction::select('id','amount')
+    return EmployeeDeduction::select('id','amortization')
         ->where('user_id',$user_id)
         ->where('deduction_id',$deduction_id)
         ->where('status','Active')
         ->whereIn('type_of_deduction',[$cut_off,'Every Cut-Off'])
-        ->sum('amount');
+        ->sum('amortization');
 }
