8ef252a9cf82de1e2d476f4b3e7e0793c430d895
Devy Viray <demetrio.viray@lfuggoc.net>
2025-01-06 08:46:27 +0800

    Fixed contribution for 2nd cut off

--- app/Http/Controllers/Payroll/PayRegController.php
+++ app/Http/Controllers/Payroll/PayRegController.php
@@ -184,6 +184,7 @@ class PayRegController extends Controller
                         $reg_ee = 0;
                         $mpf_ee = 0;
                         $reg_er = 0;
+                        $ec = 0;
                         $mpf_er = 0;
                         $month_15 = $accumulated_amount;
                         $month_30 = 0;
@@ -192,6 +193,8 @@ class PayRegController extends Controller
                         $sss_ec = 0;
                         $phic_ee = 0;
                         $hdmf_ee = 0;
+                        $month_15_phic_ee = 0;
+                        $month_15_hdmf_ee = 0;
 
                         //Get first cut off contribution
                         if($cut_off == 'Second Cut-Off'){
@@ -209,15 +212,19 @@ class PayRegController extends Controller
                                 $reg_ee = $previous_contribution->sss_reg_ee;
                                 $mpf_ee = $previous_contribution->sss_mpf_ee;
                                 $reg_er = $previous_contribution->sss_reg_er;
+                                $ec = $previous_contribution->sss_ec;
                                 $mpf_er = $previous_contribution->sss_mpf_er;
+                                $month_15_phic_ee = $previous_contribution->phic_er;
+                                $month_15_hdmf_ee = $previous_contribution->hdmf_er;
                             }
-                        }else{
-                            $sss_reg_ee = computeSSSContribution($total_accumulated,$cut_off,'employee_share_ee',$reg_ee);
-                            $sss_mpf_ee = computeSSSContribution($total_accumulated,$cut_off,'mpf_ee',$mpf_ee);
-                            $sss_ec = computeSSSecContribution($total_accumulated,$cut_off,'sss_ec',0);
-                            $phic_ee = computePHICContribution($rate,'employee_share_ee');
-                            $hdmf_ee = computePagibigContribution($rate,'employee_share_ee');
+
                         }
+                        // SSS contribution
+                        $sss_reg_ee = computeSSSContribution($total_accumulated,$cut_off,'employee_share_ee',$reg_ee);
+                        $sss_mpf_ee = computeSSSContribution($total_accumulated,$cut_off,'mpf_ee',$mpf_ee);
+                        $sss_ec = computeSSSecContribution($total_accumulated,$cut_off,'sss_ec',$ec);
+                        $phic_ee = computePHICContribution($rate,'employee_share_ee') - $month_15_phic_ee;
+                        $hdmf_ee = computePagibigContribution($rate,'employee_share_ee') - $month_15_hdmf_ee;
 
                         $ot_amount = 0;
                         $meal_allowances = 0;
