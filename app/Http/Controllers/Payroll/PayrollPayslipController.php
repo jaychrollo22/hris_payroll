5a19c8380dcfaa652ec084408671931a91d899e3
Devy Viray <demetrio.viray@lfuggoc.net>
2025-01-11 22:40:32 +0800

    Optimize viewing of payslip

--- app/Http/Controllers/Payroll/PayrollPayslipController.php
+++ app/Http/Controllers/Payroll/PayrollPayslipController.php
@@ -130,14 +130,14 @@ class PayrollPayslipController extends Controller
     public function view(PayrollRegister $payrollRegister)
     {
         $authorize = true;
-        if(checkUserPrivilege('payslip_filter_per_company',auth()->user()->id) != 'yes'){
-            $authorize = auth()->user()->id == $payrollRegister->user_id ? true : false;
-        }
+        // if(checkUserPrivilege('payslip_filter_per_company',auth()->user()->id) != 'yes'){
+        //     $authorize = auth()->user()->id == $payrollRegister->user_id ? true : false;
+        // }
 
-        if(!$authorize){
-            Alert::warning('Warning : Permission Denied!')->persistent('Dismiss');
-            return redirect()->route('payslip.password.form',['payrollRegister' => $payrollRegister->id]);
-        }
+        // if(!$authorize){
+        //     Alert::warning('Warning : Permission Denied!')->persistent('Dismiss');
+        //     return redirect()->route('payslip.password.form',['payrollRegister' => $payrollRegister->id]);
+        // }
 
         if (!session('payslip_access_granted')) {
             Alert::warning('Warning : You must enter the correct password first!')->persistent('Dismiss');
