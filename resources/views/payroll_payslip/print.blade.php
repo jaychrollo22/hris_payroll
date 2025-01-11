c9925fe983e72cec75c5e7c91cd1adf72f6c84ee
Arjay Lumagdong <jay.lumagdong@gmail.com>
2025-01-11 23:36:27 +0800

    update

--- resources/views/payroll_payslip/print.blade.php
+++ resources/views/payroll_payslip/print.blade.php
@@ -25,14 +25,19 @@
     <p style="
         text-transform: uppercase;
         text-align: center;
-        font-size: 20px;">{{ $payrollRegister->employee->company->company_name }}</p>
+        font-size: 20px;">{{ $payrollRegister->employee ? $payrollRegister->employee->company->company_name : "" }}</p>
     <div style="
         margin-bottom: 20px;
         display: block;
         margin-left: auto;
         margin-right: auto;
         width: 30%;">
-        <img  src="{{ url('company_images/'.$payrollRegister->employee->company->company_code.'.png') }}" width="200px" height="auto" alt="">
+        @if($payrollRegister->employee)
+            @if($payrollRegister->employee->company)
+                <img src="{{ url('company_images/'.$payrollRegister->employee->company->company_code.'.png') }}" width="200px" height="auto" alt="">
+            @endif
+        @endif
+        
     </div>
     <table>
         <tr>
