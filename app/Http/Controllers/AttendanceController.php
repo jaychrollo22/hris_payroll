5f846c7ee3d37b125484c85f2beb1218c77d8c4c
Arjay Lumagdong <jay.lumagdong@gmail.com>
2025-01-23 10:32:58 +0800

    update subordinates attendances

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -84,7 +84,7 @@ class AttendanceController extends Controller
         $schedule_id = null;
         $emp_data = [];
         if ($from_date != null) {
-            $emp_data = Employee::select('id','user_id','employee_number','first_name','last_name','schedule_id')
+            $emp_data = Employee::select('id','user_id','employee_number','first_name','last_name','schedule_id','work_description')
                                     ->with(['schedule_info','attendances' => function ($query) use ($from_date, $to_date) {
                                             $query->whereBetween('time_in', [$from_date." 00:00:01", $to_date." 23:59:59"])
                                                     ->orWhereBetween('time_out', [$from_date." 00:00:01", $to_date." 23:59:59"])
