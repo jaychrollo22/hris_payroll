cc3dfa8e76c061100ecc76ff9d553704ca87a90e
Arjay Lumagdong <jay.lumagdong@gmail.com>
2025-01-24 19:20:24 +0800

    update

--- app/Helpers/PayrollHelper.php
+++ app/Helpers/PayrollHelper.php
@@ -169,16 +169,16 @@ function getHDMFEr($user_id,$cutoff){
 }
 
 function computeSSSContribution($accumulated_amount,$cutoff,$field,$firstcutoff_contribution ){
-    // $highest_contribution = SssMatrixContribution::orderBy('min_salary','desc')->first();
+    $highest_contribution = SssMatrixContribution::orderBy('min_salary','desc')->first();
 
-    // if($accumulated_amount >= $highest_contribution->min_salary){
-    //     $sss_contribution = $highest_contribution;
-    // }else{
+    if($accumulated_amount >= $highest_contribution->min_salary){
+        $sss_contribution = $highest_contribution;
+    }else{
     // return $accumulated_amount . $field;
         $sss_contribution = SssMatrixContribution::where('min_salary','<=',$accumulated_amount)
             ->where('max_salary','>=',$accumulated_amount)
             ->first();
-    // }
+    }
 
     if(!$sss_contribution) return 0;
     if ($cutoff == 'Second Cut-Off') return $sss_contribution->$field - $firstcutoff_contribution;
