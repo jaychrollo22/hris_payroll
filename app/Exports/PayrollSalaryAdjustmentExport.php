4a9570218b9b50030bd095d64a82894d10dc0493
Arjay Lumagdong <jay.lumagdong@gmail.com>
2025-08-08 16:57:24 +0800

    payroll salary adjutment export

--- app/Exports/PayrollSalaryAdjustmentExport.php
+++ app/Exports/PayrollSalaryAdjustmentExport.php
@@ -59,6 +59,7 @@ class PayrollSalaryAdjustmentExport implements FromQuery, WithHeadings, WithMapp
     {
         return [
             'USER ID',
+            'PAYROLL PERIOD ID',
             'NAME',
             'COMPANY',
             // 'EFFECTIVITY DATE',
@@ -73,7 +74,8 @@ class PayrollSalaryAdjustmentExport implements FromQuery, WithHeadings, WithMapp
 
     public function map($salary_adjustment): array
     {
-        $employee_number = $salary_adjustment->employee ? $salary_adjustment->employee->employee_number : "";
+        $user_id = $salary_adjustment->employee ? $salary_adjustment->employee->user_id : "";
+        $payroll_period_id = $salary_adjustment->payroll_period_id;
         $employee_name = $salary_adjustment->employee ? $salary_adjustment->employee->last_name . ', ' . $salary_adjustment->employee->first_name . ' ' . $salary_adjustment->employee->middle_name : "";
         $company = '';
         if($salary_adjustment->employee){
@@ -81,7 +83,8 @@ class PayrollSalaryAdjustmentExport implements FromQuery, WithHeadings, WithMapp
         }
 
         return [
-            $employee_number,
+            $user_id,
+            $payroll_period_id,
             $employee_name,
             $company,
             // $salary_adjustment->effectivity_date,
